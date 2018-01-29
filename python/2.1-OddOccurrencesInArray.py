8b47b58d0625fcf9cf615c6b2f05776891b5c067
Michael Buell Jr <mikebuell@gmail.com>
2018-01-29 18:50:26 -0500

    100% solution for Problem 2.1
    Re: storage, problem states this:
    
    expected worst-case space complexity is O(1), beyond input storage
    (not counting the storage required for input arguments).
    
    Solution uses storage equal to input, so either 1 or N depending?

---
+++ python/2.1-OddOccurrencesInArray.py
@@ -0,0 +1,17 @@
+def solution(A):
+    foo = {}
+
+    # Get a count of all items in the array
+    for x in A:
+        if x in foo:
+            foo[x] += 1
+        else:
+            foo[x] = 1
+
+    # only one is odd, according to the problem statement
+    for k in foo:
+        if foo[k] % 2 != 0:
+            return k
+
+if __name__ == '__main__':
+    solution([9,3,9,3,9,7])
