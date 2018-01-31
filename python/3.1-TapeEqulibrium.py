cc4576d1d5abf76f06d36e52c117380e15918969
Michael Buell Jr <mikebuell@gmail.com>
2018-01-31 18:39:46 -0500

    Almost working, but breaks on small lists

--- python/3.1-TapeEqulibrium.py
+++ python/3.1-TapeEqulibrium.py
@@ -1,7 +1,9 @@
 # says O(N) time, but depends on how you count sum
 # It would be interesting to try in something like C
+# Edit: definitely polynomial time
 
-def solution(A):
+
+def correct_slow_solution(A):
     
     total = sum(A)
     difference = []
@@ -12,4 +14,21 @@ def solution(A):
     difference.append(abs(A[-1] - sum(A[:-1])))
 
     return min(difference)
+
+def solution(A):
+    # looks like I forgot about the last element or something because it's not 100% correct
+
+    total = sum(A)
+    remaining_total = total
+    running_total = 0
+    min_diff = 1000
+    
+    for i in range(0, len(A)):
+        remaining_total -= A[i]
+        running_total += A[i]
+        diff = abs(running_total - remaining_total)
+        if diff < min_diff:
+            min_diff = diff
+        
+    return min_diff
 
