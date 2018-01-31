f1619a662c4248280a79c474c5d9928e225cfd0d
Michael Buell Jr <mikebuell@gmail.com>
2018-01-31 16:15:13 -0500

    100% working exercise 2.1

--- python/2.2-CyclicRotation.py
+++ python/2.2-CyclicRotation.py
@@ -1,10 +1,13 @@
 def solution(A, K):
-    # write your code in Python 3.6
-    
-    if len(A) == 0 or len(A) == K:
+    """Return the list A rotated to the right K times.
+    A is a list of integers in the range [-1000,1000]
+    K is an integer between 0 and 100"""
+    if len(A) == 0 or len(A) == 1 or len(A) == K:
         return A
-        
-    K = K % len(A)
-    
-    return A[-K:] + A[:len(A)-K]
 
+    K = K % len(A)
+
+    if K == 1:
+        return A[-1:] + A[:-1]
+
+    return A[-K:] + A[:-K]
