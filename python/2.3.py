3403f5415bc008dc5929ed9a597ca16fb2fb4177
Michael Buell Jr <mikebuell@gmail.com>
2018-01-31 18:11:03 -0500

    Correct w/ correct arr len, fails on time, n^2

--- python/2.3.py
+++ python/2.3.py
@@ -1,13 +1,12 @@
+# says O(N) time, but depends on how you count sum
+# It would be interesting to try in something like C
+
 def solution(A):
-    # write your code in Python 3.6
     
     total = sum(A)
     difference = []
     
-    for i in range(len(A)-1):
-        if i == 0:
-            difference.append(abs(A[0] - sum(A[1:])))
-        else:
+    for i in range(1, len(A)-1):
             difference.append(abs(sum(A[:i]) - sum(A[i:])))
             
     difference.append(abs(A[-1] - sum(A[:-1])))
