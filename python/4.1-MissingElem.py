db1321ef06d404aee220a8d2dd0e4344b4517e08
Michael Buell Jr <mikebuell@gmail.com>
2018-02-01 02:01:17 -0500

    MissingElem 100%

---
+++ python/4.1-MissingElem.py
@@ -0,0 +1,22 @@
+def solution(A):
+    """Return 1 if A is a permutation, 0 otherwise.
+    A permutation is a sequence containing each element from 1 to N once,
+    and only once."""
+    
+    # Keep track of all digits in range
+    counters = [1] * len(A)
+
+    # Decrement counter when you see an element
+    for n in A:
+	# If element is is out of list range, it's not a permutation.
+        try:
+	    # counter should be 1, otherwise not a permutation.
+            if counters[n-1] == 1:
+                counters[n-1] -= 1
+            else:
+                return 0
+        except IndexError:
+            return 0
+    
+    return 1
+
