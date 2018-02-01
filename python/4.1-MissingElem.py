a98b696c0adb3f94da47aea5840db00b042483ce
Michael Buell Jr <mikebuell@gmail.com>
2018-02-01 02:06:04 -0500

    Notes added to 4.1

--- python/4.1-MissingElem.py
+++ python/4.1-MissingElem.py
@@ -1,3 +1,10 @@
+"""
+Naive attempt at solution using theoretical sum (N*N+1//2) vs actual sum
+got all results except the ones designed to have the correct sum, but
+are not permutations. Also, one of the extreme_values cases (all the same
+values, N = ~100,000). That was a performance test, but can't see why
+performance would be an issue.
+"""
 def solution(A):
     """Return 1 if A is a permutation, 0 otherwise.
     A permutation is a sequence containing each element from 1 to N once,
